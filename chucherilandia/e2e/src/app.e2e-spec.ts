900e34274b54712698402bda3177ae80e52894aa
Someguy300 <Someguy300@users.noreply.github.com>
2020-11-08 16:47:06 -0400

    No se que hago
    No se que hago

--- chucherilandia/e2e/src/app.e2e-spec.ts
+++ chucherilandia/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('chucheriland app is running!');
+    expect(page.getTitleText()).toEqual('chucherilandia app is running!');
   });
 
   afterEach(async () => {
--- chucherilandia/src/app/app.component.spec.ts
+++ chucherilandia/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'chucheriland'`, () => {
+  it(`should have as title 'chucherilandia'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('chucheriland');
+    expect(app.title).toEqual('chucherilandia');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('chucheriland app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('chucherilandia app is running!');
   });
 });
