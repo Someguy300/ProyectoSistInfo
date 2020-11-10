5885448c7a023f03f351ed402f57c27b5b2718ba
Manuel_Rodriguez <manuelrodriguez@correo.unimet.edu.ve>
2020-11-09 22:29:34 -0400

    Creación de navbar y otras paginas iniciales.

--- chucherilandia/src/app/app.module.ts
+++ chucherilandia/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AboutComponent } from './pages/about/about.component';
+import { EfectoComponent } from './components/efecto/efecto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent,
+    EfectoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ chucherilandia/src/app/components/efecto/efecto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-efecto',
+  templateUrl: './efecto.component.html',
+  styleUrls: ['./efecto.component.scss']
+})
+export class EfectoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
