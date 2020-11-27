9f5eed0e55b8045386e6f21e4f5b2c93e6b8b42b
Manuel_Rodriguez <manuelrodriguez@correo.unimet.edu.ve>
2020-11-27 15:43:05 -0400

    Las categorias funcionan perfectamente y ahora deberiamos tratar de arreglar que se puedan cargar varias imagenes en el producto.

--- chucherilandia/src/app/pages/admin/load-product/load-product.component.ts
+++ chucherilandia/src/app/pages/admin/load-product/load-product.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Category } from 'src/app/models/category';
 import { Product } from 'src/app/models/product';
+import { CategoryService } from 'src/app/services/category.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -12,21 +14,37 @@ import { ProductService } from 'src/app/services/product.service';
 export class LoadProductComponent implements OnInit {
   productForm: FormGroup;
   image: any;
+  categorys: Array<Category> = [];
 
   constructor(
     private fb: FormBuilder,
     private productService: ProductService,
     private router: Router,
+    private categoryService: CategoryService
   ) { }
 
   ngOnInit(): void {
     this.createForm();
+    this.getAllCategorys();
+  }
+
+  getAllCategorys(): void {
+    this.categoryService.getAllCategorys().subscribe((items) => {
+      // Setting up categorys
+      this.categorys = items.map(
+        (item) =>
+          ({
+            ...item.payload.doc.data(),
+            $key: item.payload.doc.id,
+          } as Category)
+      );
+    });
   }
 
   onSubmit(data: Product) {
-    console.log('New post', data);
+    console.log('New product', data);
     this.productService.uploadImage(data, this.image);
-    this.router.navigate(['/admin/list-product']);
+    this.router.navigate(['admin/product-list']);
   }
 
   createForm(): void {
