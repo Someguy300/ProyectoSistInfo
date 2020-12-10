f14a71833eae691bbf54a7a1918f9cc4c7af7886
gbanezca <gbanezca@gmail.com>
2020-12-10 03:24:45 -0400

    Boton paypal y validacion pago

--- chucherilandia/src/app/pages/order/order.component.ts
+++ chucherilandia/src/app/pages/order/order.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
-import { auth,User } from 'firebase';
+import { auth, User } from 'firebase';
 import { Bolsa } from 'src/app/models/bolsa';
 import { Metodoenvio } from 'src/app/models/metodoenvio/metodoenvio';
 import { Metodopago } from 'src/app/models/metodopago/metodopago.model';
@@ -12,22 +12,29 @@ import { CarritoService } from 'src/app/services/carrito.service';
 import { MetodoenvioService } from 'src/app/services/metodoenvio/metodoenvio.service';
 import { MetodopagoService } from 'src/app/services/metodopago/metodopago.service';
 import { OrderService } from 'src/app/services/order.service';
-
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
-  styleUrls: ['./order.component.scss']
+  styleUrls: ['./order.component.scss'],
 })
 export class OrderComponent implements OnInit {
+  @ViewChild('paypalElement', { static: true })
+  private paypalElement: ElementRef;
   metodosPago: Array<Metodopago> = [];
   metodosEnvio: Array<Metodoenvio> = [];
   orderForm: FormGroup;
   bolsas: Array<Bolsa> = [];
   user: User = null;
   loading = false;
-  order: Order = { user: "", total: 0 , metodoPago: "", metodoEnvio: "", bolsas: [], };
-  MetodoPagoSelected = "";
-  MetodoEnvioSelected = "";
+  order: Order = {
+    user: '',
+    total: 0,
+    metodoPago: '',
+    metodoEnvio: '',
+    bolsas: [],
+  };
+  MetodoPagoSelected = '';
+  MetodoEnvioSelected = '';
 
   constructor(
     private metodoPagoService: MetodopagoService,
@@ -37,8 +44,8 @@ export class OrderComponent implements OnInit {
     private bagService: BagService,
     private authService: AuthService,
     private orderService: OrderService,
-    private router: Router,
-  ) { }
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.getMetenvio();
@@ -46,7 +53,30 @@ export class OrderComponent implements OnInit {
     this.createForm();
     this.getBolsas();
     this.getCurrentUser();
+    window.paypal
+      .Buttons({
+        style: {
+          layout: 'horizontal',
+        },
+
+        createOrder: (data, actions) => {
+          return actions.order.create({
+            purchase_units: [
+              {
+                amount:{
+                  value: "1000",
+                  currency_code: 'USD'
+                }
+              }
+            ]
+          });
+        }
+
+      })
+      .render(this.paypalElement.nativeElement);
   }
+
+  
 
   getCurrentUser(): void {
     this.authService.getCurrentUser().subscribe((response) => {
@@ -68,7 +98,6 @@ export class OrderComponent implements OnInit {
       );
     });
   }
-
 
   getMetenvio(): void {
     this.metodoEnvioService.getAllMetenvio().subscribe((items) => {
@@ -95,13 +124,13 @@ export class OrderComponent implements OnInit {
     });
     setTimeout(() => {
       let bolsaUser = [];
-      var usuario = this.user.email
+      var usuario = this.user.email;
       this.bolsas.forEach(function (bolsa) {
         if (bolsa.user == usuario) {
-          bolsaUser.push(bolsa)
+          bolsaUser.push(bolsa);
         }
       });
-      this.bolsas = bolsaUser
+      this.bolsas = bolsaUser;
       this.loading = false;
     }, 1000);
   }
@@ -114,29 +143,35 @@ export class OrderComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log("Enviado", this.user.email)
-    var total = 0
-    for (let bolsa of this.bolsas){
-      total = total + bolsa.costoTotal 
+    console.log('Enviado', this.user.email);
+    if (this.MetodoPagoSelected != "" && this.MetodoEnvioSelected !="" ) {
+      var total = 0;
+    for (let bolsa of this.bolsas) {
+      total = total + bolsa.costoTotal;
     }
-    var usuario = this.user.email
+    var usuario = this.user.email;
     //Generando la orden
-    this.order.user = usuario
-    this.order.total = total
-    this.order.bolsas = this.bolsas
-    this.order.metodoPago = this.MetodoPagoSelected
-    this.order.metodoEnvio = this.MetodoEnvioSelected
-    console.log("ORDEN: ", this.order)
-    this.orderService.createOrder(this.order).then((res) => { });
-    this.visibilidad('#orden', false)
-    this.visibilidad('#listo', true)
+    this.order.user = usuario;
+    this.order.total = total;
+    this.order.bolsas = this.bolsas;
+    this.order.metodoPago = this.MetodoPagoSelected;
+    this.order.metodoEnvio = this.MetodoEnvioSelected;
+    console.log('ORDEN: ', this.order);
+    this.orderService.createOrder(this.order).then((res) => {});
+    this.visibilidad('#orden', false);
+    this.visibilidad('#listo', true);
     //Borrar las bolsa luego que las agrego a la orden
-    var i = 0
+    var i = 0;
     for (i = 0; i < this.bolsas.length; i++) {
       console.log(this.bolsas[i].$key);
-      this.bagService.deleteBag(this.bolsas[i].$key).then((res) => { });
+      this.bagService.deleteBag(this.bolsas[i].$key).then((res) => {});
     }
     this.carritoService.emptyCart(auth().currentUser.uid);
+
+    } else {
+      this.visibilidad('#alerta' , true)
+    }
+    
   }
 
   irHome() {
@@ -149,6 +184,4 @@ export class OrderComponent implements OnInit {
       elemento.style.display = visible ? 'block' : 'none';
     }
   }
-
-
 }
